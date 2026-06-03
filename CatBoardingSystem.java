54b2e8565f62cdc9ee13d70b47fbb87f9c9b584e
11251046-prog <11251046@student.itk.ac.id>
2026-06-03 21:37:53 +0800

    Add files via upload

---
+++ CatBoardingSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.catboardingsystem;
+
+/**
+ *
+ * @author LENOVO GAMING 3
+ */
+public class CatBoardingSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
